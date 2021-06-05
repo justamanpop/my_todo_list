e4314e7879878db2bb5faac4d8dfa66bbbab8eb2
justamanpop <pogchampoo24@gmail.com>
2021-06-05 22:08:04 +0530

    added asking for confirmation when deleting from tasklist

--- lib/UI/TaskList.dart
+++ lib/UI/TaskList.dart
@@ -3,7 +3,6 @@ import 'package:my_todo_list/UI/SqlTest.dart';
 import 'package:my_todo_list/utils/sqlutil.dart';
 import 'package:my_todo_list/Models/Task.dart';
 
-
 class TaskList extends StatefulWidget {
   const TaskList({Key? key}) : super(key: key);
 
@@ -12,7 +11,6 @@ class TaskList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TaskList> {
-
   SqlUtil sqlUtil = SqlUtil();
   int count = 0;
   ListView taskListView = ListView();
@@ -38,13 +36,15 @@ class _TaskListState extends State<TaskList> {
               MaterialPageRoute(builder: (context) => SqlTest()),
             ),
           ),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           FloatingActionButton(
             child: Icon(Icons.add),
             onPressed: () => Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => SqlTest()),
-                  // (Route<dynamic> route) => false,
+              // (Route<dynamic> route) => false,
             ),
           ),
         ],
@@ -56,49 +56,69 @@ class _TaskListState extends State<TaskList> {
     getData();
 
     return ListView.builder(
-        itemCount: count, itemBuilder: (BuildContext context, int position) {
-      return Card(
-        color: Colors.white,
-        elevation: 2.0,
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Task.colors[(tasks[position].priority)-1],
-            child: Text(Task.priorities[(tasks[position].priority)-1]),
-          ),
-          title: Text(tasks[position].name),
-          subtitle: Text(tasks[position].description),
-          trailing: IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: (){
-              deleteData(tasks[position].id);
-            },
-          ),
-          onTap: () {
-            debugPrint('I was tapped, help!, my details are card number ' +
-                (position + 1).toString());
-          },
-        ),
-      );
-    });
+        itemCount: count,
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Task.colors[(tasks[position].priority) - 1],
+                child: Text(Task.priorities[(tasks[position].priority) - 1]),
+              ),
+              title: Text(tasks[position].name),
+              subtitle: Text(tasks[position].description),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  deleteData(tasks[position].id);
+                },
+              ),
+              onTap: () {
+                debugPrint('I was tapped, help!, my details are card number ' +
+                    (position + 1).toString());
+              },
+            ),
+          );
+        });
   }
 
-  Future<void> getData() async
-  {
+  Future<void> getData() async {
     int numRows = await sqlUtil.getCountDb();
     var taskMaps = await sqlUtil.getAllTasksDb();
 
     setState(() {
       count = numRows;
-      tasks =taskMaps.map((m) => Task.fromMap(m)).toList();
+      tasks = taskMaps.map((m) => Task.fromMap(m)).toList();
     });
   }
 
-  Future<int>deleteData(int idToDelete) async {
-    int res =await sqlUtil.deleteFromDb(idToDelete);
-    debugPrint("number of rows deleted as a result were " + res.toString());
-    await getData();
-    debugPrint("before return res in deleteData");
-    return res;
-  }
+  Future<void> deleteData(int idToDelete) async {
+    AlertDialog alert = AlertDialog(
+      title: Text("Delete"),
+      content: Text("Are you sure you want to delete this task?"),
+      actions: [
+        TextButton(
+            onPressed: () {
 
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel')),
+        TextButton(
+            onPressed: () async{
+              await sqlUtil.deleteFromDb(idToDelete);
+              await getData();
+              Navigator.of(context).pop();
+            },
+            child: Text('Continue')),
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
+
+  }
 }
